1a22f2a4a9b063f5ccbbba6d1b50bb7b0117dc1e
David <jd.monsalve@uniandes.edu.co>
2020-05-11 02:28:44 -0500

    con logica back

--- js/login.js
+++ js/login.js
@@ -3,61 +3,63 @@
     "use strict";
 
 
-     /*==================================================================
-    [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    /*==================================================================
+   [ Focus input ]*/
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
-  
-  
+
+
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
-
-        return check;
+        if (check) {
+            login(input[0].value, input[1].value);
+        }
+        return false;
     });
 
-    $(document).ready(function($) {
-        $(document).on('submit', '#submit-form', function(event) {
-          event.preventDefault();
-        
-          alert('page did not reload');
-        });
-      });
+    $(document).ready(function ($) {
+        $(document).on('submit', '#submit-form', function (event) {
+            event.preventDefault();
 
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+            alert('page did not reload');
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -74,7 +76,20 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
-    
+
+    function login(username, password) {
+        fetch("http://127.0.0.1:3000/login", { method: "POST", body: JSON.stringify({ username, password }), headers: { "Accept": "application/json", "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } }).then(resp => resp.json().then(resp => {
+
+            if (resp.status == "fail") {
+                console.log(resp.status);
+                return alert(resp.message);
+            }
+
+            else {
+                location.href = "/perfil.html";
+            }
+
+        }));
+    }
 
 })(jQuery);
--- js/register.js
+++ js/register.js
@@ -4,64 +4,67 @@
 
     /*==================================================================
     [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
                 $(this).addClass('has-val');
             }
             else {
                 $(this).removeClass('has-val');
             }
-        })    
+        })
     })
 
 
     /*==================================================================
     [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
-        $(this).on('blur', function(){
-            if(validate(this) == false){
+    $('.validate-input .input100').each(function () {
+        $(this).on('blur', function () {
+            if (validate(this) == false) {
                 showValidate(this);
             }
             else {
                 $(this).parent().addClass('true-validate');
             }
-        })    
+        })
     })
 
     /*==================================================================
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function () {
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
-        return check;
+        if (check) {
+            register(input[0].value, input[1].value, input[2].value, input[3].value, input[4].value, input[5].value, input[6].value, input[7].value);
+        }
+        return false;
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-           $(this).parent().removeClass('true-validate');
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+            $(this).parent().removeClass('true-validate');
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -78,7 +81,24 @@
 
         $(thisAlert).removeClass('alert-validate');
     }
-    
+
+
+
+    function register(name, email, username, password, cellphone, direction, page, information) {
+        fetch("http://127.0.0.1:3000/register", { method: "POST", body: JSON.stringify({ name, email, username, password, cellphone, direction, page, information }), headers: { "Accept": "application/json", "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" } }).then(resp => resp.json().then(resp => {
+
+            if (resp.status == "fail") {
+                console.log(resp.status);
+                return alert(resp.message);
+            }
+
+            else {
+                location.href = "/login.html";
+            }
+
+        }));
+    }
+
 
 
 })(jQuery);
